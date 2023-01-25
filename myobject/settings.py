53e8adbce7c568c961d8d08e5750ae7948cac49b
zian <zianwang623@gmail.com>
2023-01-25 11:51:12 -0500

    change base layout

--- myobject/settings.py
+++ myobject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'osdb',
         'USER': 'root',
-        'PASSWORD': 'Sql528491!',
+        'PASSWORD': '528491',
         'HOST': 'localhost',
         'PORT': '3306',
     }
